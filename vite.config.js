1a31bb3250c7fb9fd0d773828bcedabeeefd6122
Ubil Rodrigues Arrascue <ubilrodriguesarrascue@gmail.com>
2025-03-22 04:30:47 -0500

    cambio 5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-const path = require("path");
-const { defineConfig } = require("vite");
+import path from "path";
+import { defineConfig } from "vite";
 import Banner from "vite-plugin-banner";
-import pkg from './package.json' assert { type: 'json' }; 
+import pkg from "./package.json" assert { type: "json" }; // Usar assert para JSON
 import friendlyTypeImports from "rollup-plugin-friendly-type-imports";
-module.exports = defineConfig({
+import { fileURLToPath } from "url"; // Necesario para __dirname en ES Modules
+
+// Equivalente a __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
     base: "./",
     build: {
         lib: {
@@ -12,20 +18,16 @@ module.exports = defineConfig({
             fileName: (format) => `kalidokit.${format}.js`,
         },
         rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
             exports: "named",
             external: [],
             output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
                 globals: {},
             },
         },
     },
     plugins: [
         Banner(
-            `/**\n * @${pkg.name} v${pkg.version}\n * ${pkg.description}\n * \n * @license\n * Copyright (c) ${pkg.year} ${pkg.author}\n * SPDX-License-Idntifier: ${pkg.license} \n * ${pkg.homepage}\n */`
+            `/**\n * @${pkg.name} v${pkg.version}\n * ${pkg.description}\n * \n * @license\n * Copyright (c) ${pkg.year} ${pkg.author}\n * SPDX-License-Identifier: ${pkg.license} \n * ${pkg.homepage}\n */`
         ),
         friendlyTypeImports(),
     ],
