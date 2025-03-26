1fa042a6164a87154d98c99e8d6f53abf2987bf5
Ubil Rodrigues Arrascue <ubilrodriguesarrascue@gmail.com>
2025-03-26 04:09:06 -0500

    42

--- server.js
+++ server.js
@@ -374,7 +374,12 @@ app.use((req, res, next) => {
 
 
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 
